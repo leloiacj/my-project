a103713585d60b51cc1b2b27b1c091bb60b54289
leloiacj <166014920+leloiacj@users.noreply.github.com>
2024-09-19 15:48:19 +0200

    added multyKey search

--- src/api/productSearch/controllers/productSearch.ts
+++ src/api/productSearch/controllers/productSearch.ts
@@ -4,30 +4,99 @@ export default factories.createCoreController(
   "api::product.product",
   ({ strapi }) => ({
     async searchProducts(ctx) {
-      const { query } = ctx.request.query; // Prendiamo il parametro di ricerca
+      const {
+        query,
+        title,
+        brend,
+        fuelType,
+        color,
+        gearBox,
+        carType,
+        tractionType,
+        dateOfFirstRegistration,
+        price,
+        kilometers,
+        cv,
+        littleDescription,
+        description,
+      } = ctx.request.query;
 
-      if (!query) {
-        return ctx.badRequest("Inserisci un parametro di ricerca");
+      if (
+        !query &&
+        !title &&
+        !brend &&
+        !fuelType &&
+        !color &&
+        !gearBox &&
+        !carType &&
+        !tractionType &&
+        !dateOfFirstRegistration &&
+        !price &&
+        !kilometers &&
+        !cv &&
+        !littleDescription &&
+        !description
+      ) {
+        return ctx.badRequest("Inserisci almeno un parametro di ricerca");
       }
 
       try {
-        // Query dinamica su più campi
+        const whereClause = {
+          $and: [],
+        };
+
+        if (query) {
+          whereClause.$and.push({
+            $or: [
+              { title: { $containsi: query } },
+              { description: { $containsi: query } },
+              { littleDescription: { $containsi: query } },
+              { brend: { $containsi: query } },
+              { fuelType: { $containsi: query } },
+              { color: { $containsi: query } },
+              { plate: { $containsi: query } },
+              { carType: { $containsi: query } },
+              { tractionType: { $containsi: query } },
+              { cv: { $containsi: query } },
+              { kilometers: { $containsi: query } },
+              { price: { $containsi: query } },
+            ],
+          });
+        }
+
+        if (title) whereClause.$and.push({ title: { $containsi: title } });
+        if (brend) whereClause.$and.push({ brend: { $containsi: brend } });
+        if (fuelType)
+          whereClause.$and.push({ fuelType: { $containsi: fuelType } });
+        if (color) whereClause.$and.push({ color: { $containsi: color } });
+        if (gearBox)
+          whereClause.$and.push({ gearBox: { $containsi: gearBox } });
+        if (carType)
+          whereClause.$and.push({ carType: { $containsi: carType } });
+        if (tractionType)
+          whereClause.$and.push({ tractionType: { $containsi: tractionType } });
+        if (cv) whereClause.$and.push({ cv: cv });
+        if (kilometers) whereClause.$and.push({ kilometers: kilometers });
+        if (price) whereClause.$and.push({ price: price });
+        if (littleDescription)
+          whereClause.$and.push({
+            littleDescription: { $containsi: littleDescription },
+          });
+        if (description)
+          whereClause.$and.push({ description: { $containsi: description } });
+
+        if (dateOfFirstRegistration) {
+          whereClause.$and.push({
+            dateOfFirstRegistration: { $gte: dateOfFirstRegistration },
+          });
+        }
+
         const products = await strapi.db
           .query("api::product.product")
           .findMany({
-            where: {
-              $or: [
-                { title: { $containsi: query } }, // Cerca nel campo 'title'
-                { description: { $containsi: query } }, // Cerca nel campo 'description'
-                { brend: { $containsi: query } }, // Cerca nel campo 'brend'
-                { fuelType: { $containsi: query } }, // Cerca nel campo 'fuelType'
-                { color: { $containsi: query } }, // Cerca nel campo 'color'
-                { plate: { $containsi: query } }, // Cerca nel campo 'plate'
-                { carType: { $containsi: query } }, // Cerca nel campo 'carType'
-              ],
-            },
+            where: whereClause,
             populate: {
-              image: true, // Popoliamo le immagini se necessario
+              image: true,
             },
           });
 
